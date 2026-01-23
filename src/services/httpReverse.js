2ec54c89513c79ccbe1f60102766d598094f5043
hoshiyomiX <katahiranaruse.daydream@gmail.com>
2026-01-23 13:33:40 +0800

    Create HTTP reverse proxy logic

---
+++ src/services/httpReverse.js
@@ -0,0 +1,23 @@
+import { CORS_HEADER_OPTIONS } from '../config/constants.js';
+
+export async function reverseWeb(request, target, targetPath) {
+  const targetUrl = new URL(request.url);
+  const targetChunk = target.split(":");
+
+  targetUrl.hostname = targetChunk[0];
+  targetUrl.port = targetChunk[1]?.toString() || "443";
+  targetUrl.pathname = targetPath || targetUrl.pathname;
+
+  const modifiedRequest = new Request(targetUrl, request);
+  modifiedRequest.headers.set("X-Forwarded-Host", request.headers.get("Host"));
+
+  const response = await fetch(modifiedRequest);
+  const newResponse = new Response(response.body, response);
+  
+  for (const [key, value] of Object.entries(CORS_HEADER_OPTIONS)) {
+    newResponse.headers.set(key, value);
+  }
+  newResponse.headers.set("X-Proxied-By", "Cloudflare Worker");
+
+  return newResponse;
+}
